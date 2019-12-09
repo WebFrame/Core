e6c637c79f16fc03e7a538bb5e044e02745ab261
Alex Tsvetanov <a.tsvetanov@headstarter.eu>
2019-12-09 16:49:53 +0200

    Update base.hpp

--- src/webnetpp/base.hpp
+++ src/webnetpp/base.hpp
@@ -11,7 +11,6 @@
 namespace webnetpp
 {
 	const std::string end_line = /*\r*/"\n";
-	//using path_vars = std::map < std::string, std::string >;
 	
 	struct path_vars 
 	{
@@ -83,7 +82,9 @@ namespace webnetpp
 				}
 			};
 			path_vars () {}
+		private:
 			std::vector < var > vars;
+		public:
 			const var operator[] (long long unsigned int ind) const
 			{
 				return vars[ind];
@@ -131,10 +132,8 @@ namespace webnetpp
 		}
 	};
 
-	struct response 
+	class response 
 	{
-		public:
-
 		private:
 			status_line status;
 			std::map < std::string, std::string > header;
@@ -210,7 +209,7 @@ namespace webnetpp
 	
 	using req_vars = std::map < std::string, std::string >;
 	
-	struct request
+	class request
 	{
 		private:
 		public:
@@ -306,46 +305,4 @@ namespace webnetpp
 				return std::move (res);
 			}
 	};
-/*static map < unsigned short, response > responses = 
-{"100", "Continue"},
-{"101", "Switching Protocols"},
-{"200", "OK"},
-{"201", "Created"},
-{"202", "Accepted"},
-{"203", "Non-Authoritative Information"},
-{"204", "No Content"},
-{"205", "Reset Content"},
-{"206", "Partial Content"},
-{"300", "Multiple Choices"},
-{"301", "Moved Permanently"},
-{"302", "Found"},
-{"303", "See Other"},
-{"304", "Not Modified"},
-{"305", "Use Proxy"},
-{"307", "Temporary Redirect"},
-{"400", "Bad Request"},
-{"401", "Unauthorized"},
-{"402", "Payment Required"},
-{"403", "Forbidden"},
-{"404", "Not Found"},
-{"405", "Method Not Allowed"},
-{"406", "Not Acceptable"},
-{"407", "Proxy Authentication Required"},
-{"408", "Request Time-out"},
-{"409", "Conflict"},
-{"410", "Gone"},
-{"411", "Length Required"},
-{"412", "Precondition Failed"},
-{"413", "Request Entity Too Large"},
-{"414", "Request-URI Too Large"},
-{"415", "Unsupported Media Type"},
-{"416", "Requested range not satisfiable"},
-{"417", "Expectation Failed"},
-{"500", "Internal Server Error"},
-{"501", "Not Implemented"},
-{"502", "Bad Gateway"},
-{"503", "Service Unavailable"},
-{"504", "Gateway Time-out"},
-{"505", "HTTP Version not supported"}
-; */
 }
