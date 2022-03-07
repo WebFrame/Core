7e4911302bda394f2299920f7e5ad61e7422b235
Alex Tsvetanov <alex_tsvetanov_2002@abv.bg>
2022-03-07 16:36:41 +0200

    V2 / Boost::asio for multi-thread request handling  (#3)
    * boost::asio transition
    
    Co-authored-by: Automated Publisher <actions@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
-#include <webnetpp/webnetpp.hpp>
+#include <webframe/webframe.hpp>
 #include <iostream>
 #include <sstream>
 #include <stdlib.h>
 
 int main()
 {
-	int pass = 0;
-	webnetpp::webnetpp app;
+	constexpr int fasten = webframe::webframe::init();
+	int pass = fasten;
+	webframe::webframe app;
 	app.set_static("./src/static", "/static")
 		.set_templates("./src/static/templates")
 		.handle("404", [&](std::string path) {
@@ -15,8 +16,14 @@ int main()
 		.handle("500", [&](std::string reason) {
 			return "Error 500: Internal server error: " + reason + ".";
 		})
+		.route ("/", []() { // static setup
+				return webframe::response (webframe::status_line ("1.1", "200"), {{"Content-Type", "text/html; charset=utf-8"}}, "<h1>Hello, World!</h1>");
+		})
 		.route("/{text}", [&](std::string user) {
 			return app.render("template.html", {{"username", user}});
+		})
+		.route("/favicon.ico", [&]() {
+			return "";
 		})
 		.route("/{number}", [&](int steps) {
 			for (int i = 0; i < (1 << steps); i++)
@@ -36,9 +43,7 @@ int main()
 			throw std::logic_error("asdf");
 			return "asdf";
 		});
-
 	const unsigned short port = 8888;
 	const unsigned char cores = ((std::thread::hardware_concurrency() - 1 > 0) ? (std::thread::hardware_concurrency() - 1) : 1);
-	std::cout << "Listening on :" << port << " with " << (int)cores << " cores" << std::endl;
 	app.run(port, cores);
 }
