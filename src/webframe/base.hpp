a25eea51e324dd1130e06714f0920027a690858c
Alex Tsvetanov <alex_tsvetanov_2002@abv.bg>
2022-03-26 06:41:02 +0200

    Fixes itoa implementation

--- src/webframe/base.hpp
+++ src/webframe/base.hpp
@@ -22,6 +22,67 @@ namespace webframe
 	const std::string end_line = "\n";
 
 	/** 
+	 *  @brief   Helper function for itoa
+	 *  @details Reverses a string
+	 ***********************************************/
+	void reverse(char str[], int length)
+	{
+		int start = 0;
+		int end = length -1;
+		while (start < end)
+		{
+			std::swap(*(str+start), *(str+end));
+			start++;
+			end--;
+		}
+	}
+	
+	/** 
+	 *  @brief   Implementation of itoa
+	 *  @details Converts integer to string
+	 ***********************************************/
+	char* itoa(int num, char* str, int base)
+	{
+		int i = 0;
+		bool isNegative = false;
+	
+		/* Handle 0 explicitly, otherwise empty string is printed for 0 */
+		if (num == 0)
+		{
+			str[i++] = '0';
+			str[i] = '\0';
+			return str;
+		}
+	
+		// In standard itoa(), negative numbers are handled only with
+		// base 10. Otherwise numbers are considered unsigned.
+		if (num < 0 && base == 10)
+		{
+			isNegative = true;
+			num = -num;
+		}
+	
+		// Process individual digits
+		while (num != 0)
+		{
+			int rem = num % base;
+			str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
+			num = num/base;
+		}
+	
+		// If number is negative, append '-'
+		if (isNegative)
+			str[i++] = '-';
+	
+		str[i] = '\0'; // Append string terminator
+	
+		// Reverse the string
+		reverse(str, i);
+	
+		return str;
+	}
+
+	/** 
 	 *  @brief   Type of request prameters
 	 *  @details This is the type of all parameters passed as request paramertes
 	 *  @note    Ex. In 'url?param=value' param and value will be saved in the map as key and value.
