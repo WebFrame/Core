0855d6b12981e6cfc8fa97a8c6b451082a8515a6
Alex-Tsvetanov <Alex-Tsvetanov@users.noreply.github.com>
2022-03-29 13:48:59 +0000

    Deploying to gh-pages from @ WebFrame/Core@6eed431af15418a894e41dbe6d2738ca8f0f03e6 🚀

--- docs/search/functions_3.js
+++ docs/search/functions_3.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['get_188',['get',['../d6/d55/structwebframe_1_1path__vars_1_1var.html#a0e3eb3222b90a23e10a78b287bb12e71',1,'webframe::path_vars::var::get()'],['../d0/d7e/structwebframe_1_1webframe_1_1thread__pool.html#acafd094f4bdcf55269eeea3f6cd11a48',1,'webframe::webframe::thread_pool::get()']]],
   ['get_5fbody_189',['get_body',['../d8/d06/structwebframe_1_1body__t.html#a5363b8a8f5e9e7544c19df78a9a4a4f6',1,'webframe::body_t']]],
-  ['get_5ffile_190',['get_file',['../d4/d2c/classwebframe_1_1webframe.html#a48148b2e0d85d5ab6ab5ee432ca07d6b',1,'webframe::webframe']]],
+  ['get_5ffile_190',['get_file',['../d4/d2c/classwebframe_1_1webframe.html#a8deb8f3f3d6449b277ecdc0132208089',1,'webframe::webframe']]],
   ['get_5ffree_5fthread_191',['get_free_thread',['../d0/d7e/structwebframe_1_1webframe_1_1thread__pool.html#a518a84b3126b4c3cba7df3d44229ce29',1,'webframe::webframe::thread_pool']]],
   ['get_5fin_5faddr_192',['get_in_addr',['../df/ded/host_8h.html#a294867ba9d7ff47e39d421134d8e12ab',1,'host.h']]],
   ['get_5fmime_5ftype_193',['get_mime_type',['../d6/dbd/namespacemime__types.html#a8b8d8f67609057f5faae54b23b4353b3',1,'mime_types']]],
--- docs/search/functions_f.js
+++ docs/search/functions_f.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['var_248',['var',['../d6/d55/structwebframe_1_1path__vars_1_1var.html#a9f479d16e5694ca3534911db48714a76',1,'webframe::path_vars::var::var()'],['../d6/d55/structwebframe_1_1path__vars_1_1var.html#a978b964067ed03679ac6f4fcd5c0a1e3',1,'webframe::path_vars::var::var(std::string value, std::string type=&quot;string&quot;)']]]
+  ['var_248',['var',['../d6/d55/structwebframe_1_1path__vars_1_1var.html#a9f479d16e5694ca3534911db48714a76',1,'webframe::path_vars::var::var()'],['../d6/d55/structwebframe_1_1path__vars_1_1var.html#a1781179c87c00b2eed32498e4d2dc2d1',1,'webframe::path_vars::var::var(const std::string &amp;value, const std::string &amp;type=&quot;string&quot;)']]]
 ];
