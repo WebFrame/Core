47eff31d35058a39c8b34faad36f9cebac433ef7
Alex Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2022-03-29 16:50:30 +0300

    fixing pipelines

--- src-gh-pages/docs/html/search/functions_e.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['unlock_247',['unlock',['../d5/d0d/structwebframe_1_1webframe_1_1thread.html#ab085237ab17077ad394d14c68b801d5c',1,'webframe::webframe::thread']]]
-];
--- src-gh-pages/docs/search/defines_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['export_315',['EXPORT',['../df/ded/host_8h.html#a3472d8cdbb788d5f1815b3522595bc49',1,'host.h']]]
-];
