a3c091a41d81177783968c9fbc12299ef70c7824
Alex-Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2022-03-29 13:48:59 +0000

    Deploying to gh-pages from @ WebFrame/Core@fd248d5cec2be43333c4f4cc6e145d6b9961f233 🚀

--- docs/search/all_13.js
+++ docs/search/all_13.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['value_149',['value',['../d6/d55/structwebframe_1_1path__vars_1_1var.html#a8feaff26f9b778547d6d54877419399a',1,'webframe::path_vars::var']]],
-  ['var_150',['var',['../d6/d55/structwebframe_1_1path__vars_1_1var.html',1,'webframe::path_vars::var'],['../d6/d55/structwebframe_1_1path__vars_1_1var.html#a9f479d16e5694ca3534911db48714a76',1,'webframe::path_vars::var::var()'],['../d6/d55/structwebframe_1_1path__vars_1_1var.html#a978b964067ed03679ac6f4fcd5c0a1e3',1,'webframe::path_vars::var::var(std::string value, std::string type=&quot;string&quot;)']]],
+  ['var_150',['var',['../d6/d55/structwebframe_1_1path__vars_1_1var.html',1,'webframe::path_vars::var'],['../d6/d55/structwebframe_1_1path__vars_1_1var.html#a9f479d16e5694ca3534911db48714a76',1,'webframe::path_vars::var::var()'],['../d6/d55/structwebframe_1_1path__vars_1_1var.html#a1781179c87c00b2eed32498e4d2dc2d1',1,'webframe::path_vars::var::var(const std::string &amp;value, const std::string &amp;type=&quot;string&quot;)']]],
   ['vars_151',['vars',['../d2/d7f/structwebframe_1_1path__vars.html#aa072f32ce0292c68b384834f970880f0',1,'webframe::path_vars']]],
   ['vprintf_152',['VPRINTF',['../df/ded/host_8h.html#a94e90c730d9ee7ea56e27166616bd1bb',1,'host.h']]]
 ];
