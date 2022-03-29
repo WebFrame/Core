2d7faef55d34ba3764df56439be80558850e3c76
Alex-Tsvetanov <Alex-Tsvetanov@users.noreply.github.com>
2022-03-29 01:02:53 +0000

    Deploying to gh-pages from @ WebFrame/Core@e8af782fbcd059e1c14dea6cb8396c189ff4d9b0 🚀

--- docs/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['convert_5fpath_5fto_5fregex_0',['convert_path_to_regex',['../d4/d2c/classwebframe_1_1webframe.html#a9041b13a9e7d51b643ad18e4a631dd5c',1,'webframe::webframe']]]
-];
--- docs/search/functions_8.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['main_0',['main',['../df/d0a/main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.cpp']]],
-  ['method_5fto_5fstring_1',['method_to_string',['../d8/d8d/namespacewebframe.html#a176891be494b5211d65d798cef945b6b',1,'webframe']]]
-];
