7b4bcfaac28b05c8f2a1d6f0a79b001057095a98
Alex Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2022-05-16 15:56:20 +0300

    V3 (#12)
    * src-gh-pages renamed to reports
    
    * src-gh-pages/report renamed to reports/benchmark
    
    * Contestants optimizations
    
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- benchmark/contestants/server.js
+++ benchmark/contestants/server.js
@@ -4,22 +4,20 @@ const port = 8888
 
 app.get('/:steps/1', (req, res) => {
     let x = 0;
-    for (let i = 0; i <
-        (1 << parseInt(req.params['steps'])); i += 1) {
+    for (let i = 0; i < (1 << parseInt(req.params['steps'])); i += 1) {
         x++;
     }
     res.send('Hello, World!');
-})
+});
 
 app.get('/:steps/2', (req, res) => {
     let x = 0;
-    for (let i = 0; i <
-        (1 << parseInt(req.params['steps'])); i += 1) {
-        x += Math.random() * 1e9;
+    for (let i = 0; i < (1 << parseInt(req.params['steps'])); i += 1) {
+        x += parseInt(Math.random() * 1e9);
     }
     res.send('Hello, World!');
-})
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
-})
+});
