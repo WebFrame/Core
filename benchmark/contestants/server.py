a11e866470e70212a104c68f27ef94364bc1e72b
Alex Tsvetanov <alex_tsvetanov_2002@abv.bg>
2022-05-16 15:56:20 +0300

    V3 (#12)
    * src-gh-pages renamed to reports
    
    * src-gh-pages/report renamed to reports/benchmark
    
    * Contestants optimizations
    
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- benchmark/contestants/server.py
+++ benchmark/contestants/server.py
@@ -1,21 +1,28 @@
 from flask import Flask, g
 import random
 app = Flask(__name__)
+maxInt = 2 ** 31
 
 @app.route("/<steps>/1")
 def hello(steps):
     x = 0
-    for i in range(2 ** int(steps)):
+    i = 0
+    steps = 2 ** int(steps)
+    while i < steps:
         x += 1
-        x %= 1e9
+        x %= maxInt
+        i += 1
     return "Hello World!"
 
 @app.route("/<steps>/2")
 def hello2(steps):
     x = 0
-    for i in range(2 ** int(steps)):
-        x += random.randint(0, 1e9)
-        x %= 1e9
+    i = 0
+    steps = 2 ** int(steps)
+    while i < steps:
+        x += random.randint(-2 ** 30, 2 ** 30)
+        x %= maxInt
+        i += 1
     return "Hello World!"
 
 if __name__ == "__main__":
